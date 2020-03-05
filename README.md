126d6c2de7f799bb9b3713a2d919f0439ab7a8b7
Kaprah <kevin.aprahamian@gmail.com>
2020-03-04 19:22:03 -0500

    Adding python script and readme updates

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bp-sre-interview
+
+Python script that can be run to loop through and print out the values of a dictionary with their corresponding values of the key bucketname.
+
+# Process to complete
+
+* Set AWS credentials through AWS CLI using AWS Configure
+* Created a S3 List objects function and S3 Get objects
+    * Knew the initial S3 bucket and Key, but did not know the contents of the new S3 buckets
+    * Would add a new TODO to better account for listing the objects out of the S3 buckets. After learning they all had the same name checked against that, but would want a more usable solution going forward
---
+++ boldpenguin.py
@@ -0,0 +1,41 @@
+import os
+import json
+import boto3
+
+# Boto3 clinets/Resources
+s3Client = boto3.client('s3')
+
+# S3 Get Object with key "bucket_info"
+def getS3Object(s3Client, bucketName, bucketKey):
+    response = s3Client.get_object(
+        Bucket=bucketName,
+        Key=bucketKey
+    )
+    return response
+
+# S3 list objects in particular bucket
+def listS3Objects(s3Client, key):
+    response = s3Client.list_objects_v2(
+        Bucket=key
+    )
+    return response
+
+def main():
+    bucketName = "bold-penguin-rookery-interview-puzzle-b-keybucket-fjd4blhrhjf7"
+    bucketKey  = "bucket_info"
+    initialInfo = getS3Object(s3Client, bucketName, bucketKey)
+    initialBody = initialInfo['Body'].read().decode('utf-8')
+    dictInitialBody = json.loads(initialBody)
+    for key, value in dictInitialBody.items():
+        # Check Contents of bucket to see files that are in the bucket
+        listBucketObjects = listS3Objects(s3Client, key)
+        for bucketObject in listBucketObjects['Contents']:
+            if(bucketObject['Key'] == "bucket_info"):
+                bucketInformation = getS3Object(s3Client, key, bucketObject['Key'])
+        bucketBody = bucketInformation['Body'].read().decode('utf-8')
+        dictBucketBody = json.loads(bucketBody)
+        for bucketKey, bucketValue in dictBucketBody.items():
+            if(value == bucketKey):
+                print(value, bucketValue)
+
+main()
